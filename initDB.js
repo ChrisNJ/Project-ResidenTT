53cbd18b66beeb0f252057ba25bfd2e3290593a8
ChrisNJ <39505428+ChrisNJ@users.noreply.github.com>
2021-04-15 00:21:40 -0400

    Update initDB.js

--- initDB.js
+++ initDB.js
@@ -8,20 +8,28 @@ const CrimeReport = require("./models/CrimeReport");
 
 // import xlsx to read the excel file and convert the columns to json format
 const XLSX = require("xlsx");
-const workbook = XLSX.readFile("crimereports1.xlsx");
+const workbook = XLSX.readFile("crimereports.xlsx");
 const sheet_name_list = workbook.SheetNames;
 crimereports = XLSX.utils.sheet_to_json(workbook.Sheets[sheet_name_list[0]]);
 
 // reads the json object and creates records in the CrimeReport table
-for (var i = 0; i < crimereports.length; i++) {
-  CrimeReport.create({
-    date: new Date(crimereports[i].date),
-    time: crimereports[i].time,
-    station: crimereports[i].station,
-    division: crimereports[i].division,
-    location: crimereports[i].location,
-    latitude: crimereports[i].latitude,
-    longitude: crimereports[i].longitude,
-    offences: crimereports[i].offences,
-  });
-}
+var uploadFile = function () {
+  try {
+    for (var i = 0; i < crimereports.length; i++) {
+      CrimeReport.create({
+        date: crimereports[i].date,
+        time: crimereports[i].time,
+        station: crimereports[i].station,
+        division: crimereports[i].division,
+        location: crimereports[i].location,
+        latitude: crimereports[i].latitude,
+        longitude: crimereports[i].longitude,
+        offences: crimereports[i].offences,
+      });
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+uploadFile();
